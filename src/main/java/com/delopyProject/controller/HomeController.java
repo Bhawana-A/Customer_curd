7842d956941021374dbedf6e1ea92ddc2b826a75
Bhawana <bhawanaahirwar52@gmail.com>
2026-06-02 10:32:54 +0530

    add HomeController

---
+++ src/main/java/com/delopyProject/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.delopyProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Customer CRUD API Running Successfully";
+    }
+}
